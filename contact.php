9b872b3b77e1f924cb138c245eec20707d765e66
MichealGSSmith <w0777761@myscc.ca>
2022-04-21 04:48:17 -0400

    some php
    did some of the php script need to finish that next

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+
+function sanitize_my_email($field) {
+    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
+    if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+$to_email = 'w0777761@myscc.ca';
+$subject = 'Testing PHP Mail';
+$message = 'Someone is inquiring about Aisha Senior Living';
+$headers = 'From: noreply @ company. com';
+//check if the email address is invalid $secure_check
+$secure_check = sanitize_my_email($to_email);
+if ($secure_check == false) {
+    echo "Invalid input";
+} else { //send email 
+    mail($to_email, $subject, $message, $headers);
+    echo "Email Sent";
+}
