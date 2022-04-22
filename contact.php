31c32a901cbc931bae73b41158889f832b30dfe7
MichealGSSmith <w0777761@myscc.ca>
2022-04-22 13:14:37 -0400

    finished the php and contact
    i finished all my php added some name to the form so i can call it in php and set up the php so client will recieve a bettter formatted email

--- contact.php
+++ contact.php
@@ -1,22 +1,40 @@
 <?php
 
-function sanitize_my_email($field) {
-    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
-    if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
-        return true;
-    } else {
-        return false;
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+ 
+
+    function sanitize_my_email($email) {
+        $field = filter_var($email, FILTER_SANITIZE_EMAIL);
+        if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
-$to_email = 'w0777761@myscc.ca';
-$subject = 'Testing PHP Mail';
-$message = 'Someone is inquiring about Aisha Senior Living';
-$headers = 'From: noreply @ company. com';
-//check if the email address is invalid $secure_check
-$secure_check = sanitize_my_email($to_email);
-if ($secure_check == false) {
-    echo "Invalid input";
-} else { //send email 
-    mail($to_email, $subject, $message, $headers);
-    echo "Email Sent";
+    $to_email = 'w0777761@myscc.ca';
+    $body = "";
+
+    $body .= 'From: ' . $name. "\r\n";
+    $body .= 'Email: ' . $email. "\r\n";
+    $body .= 'Subject: ' . $subject. "\r\n";
+    $body .= 'Message: ' . $message."\r\n";
+
+    //check if the email address is invalid $secure_check
+    $secure_check = sanitize_my_email($email);
+     if ($secure_check == false) {
+        echo "Invalid input";
+    } else { //send email 
+        mail($to_email, $email, $body);
+        echo "Email Sent";
+         header('location: contact.html');
+
+        
+    } 
+
+   
+
 }
